70c8355f6b1f1e041fce5bd6f1bedd446d583c59
Cesur Bayram <cesr.bayram@gmail.com>
2023-12-29 17:15:50 +0300

    string-karekter-degistirme

---
+++ 40-string-karekter-degistirme.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    char dizim[] ="Cesur Bayram";
+
+    dizim[0]='R';
+    printf("%s",dizim);//string içinde karekter degisimi yaptım
+    return 0; 
+}
