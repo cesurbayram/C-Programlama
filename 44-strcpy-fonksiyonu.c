45dc048380fb340e7585f431a6c92ded79ee2ca7
Cesur Bayram <cesr.bayram@gmail.com>
2023-12-30 16:08:44 +0300

    strcpy-fonksiyonu

---
+++ 44-strcpy-fonksiyonu.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+        char can[]="Benim adim Cesur Bayram";
+        char canan[100];
+
+        strcpy(canan,can);
+
+        printf("%s",canan);
+
+    return 0;
+}
