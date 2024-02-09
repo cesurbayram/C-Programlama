f9b8800bc045a3eba277183b354b5cc8e7fb8db4
Cesur Bayram <cesr.bayram@gmail.com>
2024-02-09 16:08:18 +0300

    dizinin-elemanini-fonksiyon-ile-bulmak

---
+++ 132-dizinin-elamanini-bulmak-fonksiyon18.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void ekle(int A[], int sirano, int deger, int elemansayisi)
+{
+    int i;
+    if (sirano <= elemansayisi + 1)
+    {
+        for (i = elemansayisi; i >= sirano; i--)
+        {
+            A[i] = A[i - 1];
+        }
+        A[sirano - 1] = deger;
+
+        printf("Dizimizin eklenmis hali\n");
+        for (i = 0; i < elemansayisi + 1; i++)
+        {
+            printf("%d ", A[i]);
+        }
+
+        printf("\n");
+    }
+    else
+    {
+        printf("Gecerli bir sira numarasi giriniz\n");
+    }
+}
+
+int main()
+{
+    int i, sirasi;
+    int n, deger;
+    printf("kac adet sayi uretilecek\n");
+    scanf("%d", &n);
+
+    int *dizi = (int *)malloc(n * sizeof(int));
+
+    srand(time(0));
+    for (i = 0; i < n; i++)
+    {
+        dizi[i] = rand() % 100;
+    }
+
+    printf("Hangi siraya eleman eklenilecek\n");
+    scanf("%d", &sirasi);
+    printf("eklenilecek sayi nedir\n");
+    scanf("%d", &deger);
+
+    ekle(dizi, sirasi, deger, n);
+
+    free(dizi);
+
+    return 0;
+}
