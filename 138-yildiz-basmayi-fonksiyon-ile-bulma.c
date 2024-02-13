5355c1bdc39c9443ae5dd5080373f5e2bf5a71fb
Cesur Bayram <cesr.bayram@gmail.com>
2024-02-13 14:40:29 +0300

    modified fonksiyon egzersiz

--- 138-yildiz-basmayi-fonksiyon-ile-bulma.c
+++ 138-yildiz-basmayi-fonksiyon-ile-bulma.c
@@ -3,34 +3,47 @@
 
 void sekilciz(int kenar, char karakter)
 {
-        int i,j;
-        for (j = kenar; j > 0 ; j--)
+    int i, j;
+
+    for (j = kenar; j > 0; j--)
+    {
+        for (i = 1; i <= kenar - j; i++)
         {
-            for ( i = 1; i <= kenar-j; i++)
-            {
-                printf(" ");
-            }
-            for ( i = 1; i <= 2; i++)
-            {
-                printf("%d");
-            }
-            
-            printf("\n");
-        }  
+            printf(" ");
+        }
+        for (i = 1; i <= 2 * j - 1; i++)
+        {
+            printf("%d", i);  // Print the loop variable i instead of a constant number
+        }
+
+        printf("\n");
+    }
+
+    for (j = 1; j <= kenar; j++)
+    {
+        for (i = 1; i <= kenar - j; i++)
+        {
+            printf(" ");
+        }
+        for (i = 1; i <= 2 * j - 1; i++)
+        {
+            printf("%c", karakter);
+        }
+        printf("\n");
+    }
 }
-
-
 
 int main()
 {
     int kenar;
     char karakter;
-    printf("Lutfen basilacak karekter giriniz\n");
-    scanf("%c",&karakter);
+
+    printf("Lutfen basilacak karakter giriniz\n");
+    scanf(" %c", &karakter);  // Add a space before %c to consume the newline character
     printf("Kenar sayisi\n");
-    scanf("%d",&kenar);
+    scanf("%d", &kenar);
 
     printf("\n\n\n");
-    sekilciz(kenar,karakter);
+    sekilciz(kenar, karakter);
     return 0;
 }
